61d91620c58352dcb535ee7cf1d32ed58323bf98
noway0032 <noway0032@gmail.com>
2020-10-18 18:43:03 +0200

    module start

---
+++ A8mwtfamt/Course/module/marh.util.ts
@@ -0,0 +1,19 @@
+function add(a, b): number{
+    return a + b;
+}
+
+function subtract(a, b): number{
+    return a - b;
+}
+
+function multiply(a, b): number{
+    return a * b;
+}
+
+function divide(a, b): number{
+    return a / b;
+}
+
+function log() {
+    console.log(`Math logged!`);
+}
